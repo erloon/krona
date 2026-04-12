b3220e420db7f9a33d84e552d5300fb746a0aaeb
Maciej Kryca <Maciej Kryca>
2026-04-12 22:26:34 +0200

    feat(env): add TypeScript definitions for environment variables related to Google client IDs

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID: string;
+    EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID: string;
+    EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID: string;
+  }
+}
